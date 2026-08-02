7d11c1e9330691ac02482336a797b4e21a868bd2
Ngyn05 <hnguyenhuynh926@gmail.com>
2026-08-02 19:11:38 +0700

    fix bug

--- wp-content/themes/vasco-theme/functions.php
+++ wp-content/themes/vasco-theme/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'VASCO_THEME_VERSION', '1.0.0' );
+define( 'VASCO_THEME_VERSION', '1.0.1' );
 define( 'VASCO_THEME_DIR', get_template_directory() );
 define( 'VASCO_THEME_URI', get_template_directory_uri() );
 
