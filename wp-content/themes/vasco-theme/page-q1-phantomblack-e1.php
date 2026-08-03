becf75dbd6c032aec1f13c3215c4ad32d54710ed
Ngyn05 <hnguyenhuynh926@gmail.com>
2026-08-03 10:56:46 +0700

    fix

--- wp-content/themes/vasco-theme/page-q1-phantomblack-e1.php
+++ wp-content/themes/vasco-theme/page-q1-phantomblack-e1.php
@@ -20,13 +20,13 @@ get_header();
 <nav aria-label="Breadcrumbs" class="breadcrumb" data-depth="3">
 <ol>
 <li>
-<a href="../index.html">
+<a href="<?php echo esc_url( home_url( "/" ) ); ?>">
 <span class="breadcrumb-link">Trang chủ</span>
 </a>
 <span class="breadcrumb-divider">&gt;</span>
 </li>
 <li>
-<a href="../index.html">
+<a href="<?php echo esc_url( home_url( "/translators/" ) ); ?>">
 <span class="breadcrumb-link">Máy phiên dịch điện tử</span>
 </a>
 <span class="breadcrumb-divider">&gt;</span>
