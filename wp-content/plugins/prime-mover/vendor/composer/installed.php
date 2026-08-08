92cc03a85a15c305fcc12fff968ab757b055b9ed
Ngyn05 <hnguyenhuynh926@gmail.com>
2026-08-08 13:37:13 +0700

    feat: add prime-mover plugin and configure gitignore to exclude large export files

---
+++ wp-content/plugins/prime-mover/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codex-m/prime-mover-core',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd93c3ed24b8bc25d8ef2f8a8d6c80e46cf7efa13',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codex-m/prime-mover-core' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd93c3ed24b8bc25d8ef2f8a8d6c80e46cf7efa13',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
